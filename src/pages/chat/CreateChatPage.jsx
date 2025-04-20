cefdc81318432f88c4ee60348e0fcb08f5f854b1
sj0919 <seungjinlee09@gmail.com>
2025-04-20 11:14:35 +0900

    ♻️ button오류 수정

--- src/pages/chat/CreateChatPage.jsx
+++ src/pages/chat/CreateChatPage.jsx
@@ -218,7 +218,7 @@ const CreateChatPage = () => {
           />
         </ItemContainer>
       </Container>
-      <BottomButton text="등록" />
+      <BottomButton text="등록" onClick={handleCreateChat} />
     </Layout>
   );
 };
